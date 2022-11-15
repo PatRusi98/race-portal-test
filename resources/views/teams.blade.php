c0f73a9d58962de50f6fb737de6f1e4b5195fc1f
Patrik <patrik.rusinak@gmail.com>
2022-11-15 20:48:05 +0100

    added buttons

--- resources/views/teams.blade.php
+++ resources/views/teams.blade.php
@@ -1,6 +1,7 @@
 @extends('rp_components.master')
 
 @section('content')
+    <button type="button" class="btn btn-success">Add</button>
     <table class="table table-striped table-bordered">
         <thead>
         <tr>
@@ -8,7 +9,7 @@
             <th scope="col">Team Name</th>
             <th scope="col">Short</th>
             <th scope="col">Manager</th>
-            <th scope="col">Action</th>>
+            <th scope="col">Action</th>
         </tr>
         </thead>
         <tbody class="table-group-divider">
@@ -19,6 +20,7 @@
                     <td>{{$team->name}}</td>
                     <td>{{$team->short}}</td>
                     <td>{{$team->manager}}</td>
+                    <td><button type="button" class="btn btn-primary">Edit</button><button type="button" class="btn btn-danger">Delete</button></td>
                 </tr>
             @endforeach
         </tr>
